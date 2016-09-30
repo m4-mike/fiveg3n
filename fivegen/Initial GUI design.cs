eaba74545b080f933e40704c0bbb80126d432f3a
Adam Bruce <adam.bruce397@gmail.com>
2016-09-30 12:36:50 +0930

    Broke about text into multiple lines to fit on screen,

--- fivegen/Initial GUI design.cs
+++ fivegen/Initial GUI design.cs
@@ -119,7 +119,14 @@ namespace WindowsFormsApplication1
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("TetGen is a powerful command-line tool with many options for generating tetrahedral lattices. These lattices are used for many applications from mechanical engineering, biomedical research, fluid simulations, to computer games and interactive VR simulations. Their exact structure is very important for different applications, and TetGen is one of very few open-source tools available that have these features. http://wiasberlin.de/software/tetgen/ \n \nThis project will develop a graphical user interface that simplifies the use of this tool(using presets and thumbnails etc) to open up the tool to a far bigger audience.It will include 3D computer graphics to visualize the generated 3D meshes (both triangle-shell meshes, and tetrahedral lattices).",
+            MessageBox.Show("TetGen is a powerful command-line tool with many options for generating tetrahedral lattices. " +
+                "These lattices are used for many applications from mechanical engineering, biomedical research, fluid simulations, " +
+                "to computer games and interactive VR simulations. Their exact structure is very important for different applications," +
+                " and TetGen is one of very few open-source tools available that have these features. " +
+                "http://wiasberlin.de/software/tetgen/" +
+                " \n \nThis project will develop a graphical user interface that simplifies the use of this tool" +
+                "(using presets and thumbnails etc) to open up the tool to a far bigger audience.It will include 3D " +
+                "computer graphics to visualize the generated 3D meshes (both triangle-shell meshes, and tetrahedral lattices).",
             "About");
         }
 
